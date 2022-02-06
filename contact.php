b8ab8cc6aa5c6326707e59cd41c63771ff28c3a8
aliyumbutu <39166994+aliyumbutu@users.noreply.github.com>
2022-02-06 22:50:18 +0530

    24/10/2021
    Created By: Aliyu Muhammad Butu
    from: Adamawa State, Nigeria
    Email: aliyumuhammadbutu@gmail.com
    
    Farmcart - Farmers Vegetable Ecommerce Platform
    
    // Project North Indian Biggest hackerthon 2021
    
    User Login Credentials
    User: admin@farmcart.io
    Password: Admin
    
    Database Credentials
    
    MySQL Host: localhost
    MySQL DB Name:farmcart
    MySQL User Name: farmcart_user
    Mysql Password: onCgMai19wJ

---
+++ contact.php
@@ -0,0 +1,79 @@
+<?php require_once("head.php") ?>
+
+ <section id="hero">
+    <div id="heroCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+
+      <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+      <div class="carousel-inner" role="listbox">
+
+        <!-- Slide 1 -->
+        <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-1.jpg)">
+          <div class="carousel-container">
+            <div class="container">
+              <h2 class="animate__animated animate__fadeInDown">  <span></span></h2>      
+            </div>
+          </div>
+        </div>
+
+        <!-- Slide 2 -->
+        <div class="carousel-item" style="background-image: url(assets/img/slide/slide-2.jpg)">
+          <div class="carousel-container">
+            <div class="container">
+              <h2 class="animate__animated animate__fadeInDown"></h2>
+            </div>
+          </div>
+        </div>
+
+        <!-- Slide 3 -->
+        <div class="carousel-item" style="background-image: url(assets/img/slide/slide-3.jpg)">
+          <div class="carousel-container">
+            <div class="container">
+              <h2 class="animate__animated animate__fadeInDown"></h2>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <a class="carousel-control-prev" href="#heroCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon icofont-simple-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+
+      <a class="carousel-control-next" href="#heroCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon icofont-simple-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+
+    </div>
+  </section><!-- End Hero -->
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-3">
+			
+		</div>
+		<div class="col-md-6">
+
+			<hr>
+
+			<center><h1>Innovative Coders</h1>
+
+				<h3>2021 Hackathon</h3>
+
+				<h4>7568642874</h4>
+
+				<hr>
+
+			</center>
+			
+		</div>
+		<div class="col-md-3">
+			
+		</div>
+	</div>
+</div>
+  
+
+<?php require_once("footer.php"); ?>
